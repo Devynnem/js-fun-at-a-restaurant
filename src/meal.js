123db83460de7d0ba64fe7d49116ce396e1eab73
Devynne Marshall <devynnem@gmail.com>
2023-02-03 21:49:30 -0700

    Show my meal.js funtions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+};
 
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  }
+  return menuItem
+}
+
+function addIngredients(toppings, ingredients ) {
+  if (!ingredients.includes(toppings)) {
+    ingredients.push(toppings)
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+
+}
+
+function decreasePrice(price) {
+  return price*.9
+  }
+
+function createRecipe(title, ingredients, menuItemType) {
+  return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
