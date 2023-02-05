f629a07be0b0237264935a4c6b4999dd3c338c91
Devynne Marshall <devynnem@gmail.com>
2023-02-04 17:02:07 -0700

    Take me to my progress during restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,28 @@
+function createRestaurant(name) {
+  var restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+
+  return restaurant  
+
+}
+
+function addMenuItem (pizzaRestaurant, item) {
+ if (item.type === "lunch") {
+  pizzaRestaurant.menus.lunch.push(item)
+ } else {
+    pizzaRestaurant.menus.breakfast.push(item)
+   }
+ }
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
